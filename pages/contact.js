83510c8f492b8c1fa1bdc678e91d9deadb6d39a8
Bhavin Africawala <anju.tailor89@gmail.com>
2020-11-18 14:59:23 -0800

    add footer to other pages

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,13 @@
 import MenuBar from './components/menubar';
 import React from 'react';
+import Footer from './components/footer';
 
 const Contact = () => {
     return (
         <div>
             <header className="top-navbar">
                 <MenuBar pagename="contact" />
+                <Footer />
             </header>
         </div>
     );
--- pages/menu.js
+++ pages/menu.js
@@ -1,11 +1,13 @@
-import MenuBar from './components/menubar';
 import React from 'react';
+import MenuBar from './components/menubar';
+import Footer from './components/footer';
 
 const Menu = () => {
     return (
         <div>
             <header className="top-navbar">
                 <MenuBar pagename="menu" />
+                <Footer />
             </header>
         </div>
     );
